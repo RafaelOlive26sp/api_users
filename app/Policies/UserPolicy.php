a95a1d215e3d0d6e2b0b52071b3c7718a3f052c1
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-09 22:26:22 -0300

    created, Policies, viewAny

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class UserPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function viewAny(User $user):bool
+    {
+        return in_array($user->privilege_id,[1,2]);
+
+    }
+}
