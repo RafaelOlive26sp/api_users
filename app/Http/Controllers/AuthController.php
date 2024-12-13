424c6f63e89b639b1d0edb62747804a87e267813
Rafael <oliveirarafael22@outlook.com.br>
2024-12-13 03:49:57 -0300

    Modiff, Metodos, swagger, login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,6 +29,7 @@ use OpenApi\Annotations as OA;
  *      description="Demo API Server"
  * )
  *
+ *
  */
 class AuthController extends Controller
 {
@@ -51,54 +52,59 @@ class AuthController extends Controller
      *                  type="string",
      *
      *                  example="fulan@teste.com",
-     *                  description="d"
+     *                  description="Input do email do login"
      *              ),
      *              @OA\Property(
      *                  property="password",
      *                  type="string",
      *
      *                  example="password",
-     *                  description="d"
+     *                  description="Input da Senha da conta"
      *              )
      *        )
      *     ),
      *     @OA\Response(
      *         response=200,
-     *          description="k",
-     *          @OA\JsonContent(
-     *              @OA\Property(
-     *                  property="id",
-     *                  type="integer",
-     *                  example="4",
-     *                  description="d"
-     *              ),
-     *              @OA\Property(
-     *                  property="name",
-     *                  type="string",
-     *                  example="fulano",
-     *                  description="d"
-     *              ),
-     *              @OA\Property(
-     *                  property="email",
-     *                  type="string",
-     *                  example="fulan@teste.com",
-     *                  description="d"
-     *              ),
+     *         description="Login realizado com sucesso",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="access_toke", type="string", example="000|pnQZ3VxhbHU49DWTycJOvZgLaKob6RB5Gn8zVtZsb9b7455a"),
+     *              @OA\Property(property="token_type", type="string", example="Bearer"),
+
+     *             )
      *
-     *          )
      *     ),
-     *     @OA\Response(
+     *    @OA\Response(
      *         response=401,
-     *          description="d",
-     *          @OA\JsonContent(
-     *              @OA\Property(
-     *                  property="message",
-     *                  type="string",
-     *                  example="Unauthorized",
-     *                  description="d"
-     *              )
-     *          )
-     *     )
+     *         description="Unauthorized",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Unauthorized")
+     *         )
+     *     ),
+     *    @OA\Response(
+     *         response=422,
+     *         description="Campos obrigatórios não foram preenchidos. Caso um ou mais campos sejam omitidos, será exibida uma mensagem de erro.",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="O campo email é Obrigatorio",
+     *                 description="Mensagem de erro quando um campo obrigatório não é enviado."
+     *            ),
+     *             @OA\Property(
+     *                     property="errors",
+     *                     type="object",
+     *                 @OA\Property(
+     *                     property="title",
+     *                     type="array",
+     *                     @OA\Items(
+     *                         type="string",
+     *                         example="O campo email é Obrigatorio",
+     *                         description="Lista os erros específicos do campo."
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     ),
      *
      *)
      */
@@ -132,6 +138,36 @@ class AuthController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+
+     /**
+     * @OA\Post(
+     *     path="/logout",
+     *     summary="Realiza logout do usuário",
+     *     tags={"Autenticação"},
+     *     security={{"sanctumAuth":{}}},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Logout realizado com sucesso",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *               property="message",
+     *               type="string",
+     *               example="Logged out")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Não autenticado",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message",
+     *             type="string",
+     *             example="Unauthenticated")
+     *         )
+     *     )
+     * )
+     */
+
+
     public function logout (Request $request)
     {
         $request->user()->tokens()->delete();
@@ -139,6 +175,75 @@ class AuthController extends Controller
             'message' => 'Logged out'
         ],200);
     }
+
+
+
+    /**
+     * @OA\Post(
+     *     path="/register",
+     *     summary="Realiza o cadastro de Usuarios.",
+     *     tags={"Usuario"},
+     *     description="Realiza o cadastro de usuarios para acesso ao sistema .",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"name","email", "password"},
+     *             @OA\Property(property="name", type="string", format="name", example="Edward"),
+     *             @OA\Property(property="email", type="string", format="email", example="junior@t.com"),
+     *             @OA\Property(property="password", type="string", format="password", example="password"),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Conta Criada com sucesso",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="data", type="object",
+     *                  @OA\Property(property="id", type="integer", example="21"),
+     *                  @OA\Property(property="name", type="string", example="edward"),
+     *                  @OA\Property(property="email", type="string", example="edward@tete.com"),
+     *                  @OA\Property(property="privilege_id", type="string", example="null",description="Por Padrao todos os usuarios teram o privilegio de 3, que significa cliente, no momento o valor apresentado sera como null, mas posteriormente ele é alterado"),
+     *
+     *              )
+
+     *          )
+
+     *      ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Campos obrigatórios não foram preenchidos. Caso um ou mais campos sejam omitidos, será exibida uma mensagem de erro.",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="O campo nome é Obrigatorio (and 1 more error)",
+     *                 description="Mensagem de erro quando um campo obrigatório não é enviado."
+     *            ),
+     *             @OA\Property(
+     *                     property="errors",
+     *                     type="object",
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="array",
+     *                     @OA\Items(
+     *                         type="string",
+     *                         example="O campo Name é Obrigatorio",
+     *                         description="Se o campo for emitido, sera exibido o erro no campo especifico."
+     *                     )
+     *                 ),
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="array",
+     *                     @OA\Items(
+     *                         type="string",
+     *                         example="Este email ja esta em uso!",
+     *                         description="Caso, o email que o usuario esteja tentando cadastrar ja exista."
+     *                     )
+     *                 )
+     *             )
+     *         )
+     *     ),
+     * )
+     */
 
     public function register(StoreUserRequest $request)
     {
