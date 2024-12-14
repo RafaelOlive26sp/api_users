3f690c6c73610b66223aa6ee99ea99c6f29bd110
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-14 11:20:45 -0300

    Added, prefix group version v1 in routes

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+Route::prefix('v1')->group(function () {
 
 Route::resource('users', UserController::class)->middleware(['auth:sanctum']);
 
@@ -14,3 +14,5 @@ Route::post('/login',[AuthController::class,'login']);
 Route::post('/register',[AuthController::class,'register']);
 
 Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+});
+
