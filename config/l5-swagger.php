2284bd554eb9f1866084fc1b03a93c7573ee91ae
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-15 18:54:03 -0300

    Moddif, swagger, config, URL

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -19,6 +19,11 @@ return [
                 /*
                  * Edit to include full URL in ui for assets
                  */
+                'swagger_ui' => [
+                    'url' => env('SWAGGER_URL', 'https://api-users-rafael.up.railway.app/api/documentation'),  // Ensure HTTPS is used
+                    'path' => env('SWAGGER_UI_PATH', 'swagger-ui'),
+                ],
+
                 'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', true),
 
                 /*
