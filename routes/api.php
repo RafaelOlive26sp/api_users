77d8252e1fbb7e04427cfab7dc9218cf3804262d
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-15 18:21:39 -0300

    Added, names the routes

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,17 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 Route::prefix('v1')->group(function () {
 
 Route::resource('users', UserController::class)->middleware(['auth:sanctum']);
 
 
 
-Route::post('/login',[AuthController::class,'login'])->middleware(['throttle:30,1']);
-Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login'])->middleware(['throttle:30,1'])->name('login');
+Route::post('/register',[AuthController::class,'register'])->name('register');
 
-Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum')->name('logout');
 });
 
