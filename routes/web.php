1ee2dbf09841ae15646c5903cb6d7b5648409b91
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-15 20:54:40 -0300

    Modiff, test. router

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/docs/api-docs.json', function () {
+    $filePath = storage_path('app/api-docs/api-docs.json');
+
+    if (file_exists($filePath)) {
+        return response()->json(json_decode(file_get_contents($filePath)));
+    }
+
+    return response()->json(['error' => 'Arquivo não encontrado'], 404);
+});
