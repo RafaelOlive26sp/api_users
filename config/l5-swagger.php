32e7ab900615d12db26e79ee87ca6caf4035d92a
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-15 20:26:15 -0300

    Modiff, address api-docs

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -84,7 +84,7 @@ return [
             /*
              * Absolute path to location where parsed annotations will be stored
              */
-            'docs' => storage_path('api-docs'),
+            'docs' => storage_path('storage/api-docs/api-docs'),
 
             /*
              * Absolute path to directory where to export views
