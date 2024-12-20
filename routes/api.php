f362cc00bbc40b80213fcec88ea325cb6364b265
Rafael Delgado <oliveirarafael22@outlook.com.br>
2024-12-19 21:04:08 -0300

    Added, Route for verification.resend

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmailVerificationController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group(function () {
 
 Route::resource('users', UserController::class)->middleware(['auth:sanctum']);
+Route::post('/email/resend', [EmailVerificationController::class, 'resend'])->middleware('throttle:6,1')->name('verification.resend');
 
 
 
@@ -16,5 +18,6 @@ Route::post('/login',[AuthController::class,'login'])->middleware(['throttle:30,
 Route::post('/register',[AuthController::class,'register'])->middleware(['throttle:5,1'])->name('register');
 
 Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum')->name('logout');
+
 });
 
