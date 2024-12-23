9f8b11f02c93d3f3203c2352708a3e14111fa3de
Rafael <oliveirarafael22@outlook.com.br>
2024-12-23 03:40:07 -0300

    feat: middleware to log user actions and request data

---
+++ app/Http/Middleware/LogActions.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\ActionLog;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Notifications\Action;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogActions
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+            ActionLog::create([
+                'user_id' => auth()->id(),
+                'action' => $request->method(),
+                'endpoint' => $request->path(),
+                'request_data' => $request->all(),
+                'response_data' => $response->getContent(),
+                'ip_address' => $request->ip(),
+            ]);
+            return $response;
+
+    }
+}
