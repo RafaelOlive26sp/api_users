046f33419a0fbcf2d1d82764263b98532435d8e0
Rafael <oliveirarafael22@outlook.com.br>
2024-12-27 03:34:00 -0300

    feat: add route for user statistics data retrieval

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EmailVerificationController;
+use App\Http\Controllers\StatisticsDataUsersController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,7 +12,7 @@ Route::prefix('v1')->group(function () {
 
 Route::resource('users', UserController::class)->middleware(['auth:sanctum']);
 Route::post('/email/resend', [EmailVerificationController::class, 'resend'])->middleware('throttle:6,1')->name('verification.resend');
-
+Route::get('statisticdata',[StatisticsDataUsersController::class, 'index'])->middleware('auth:sanctum')->name('data');
 
 
 Route::post('/login',[AuthController::class,'login'])->middleware(['throttle:30,1'])->name('login');
